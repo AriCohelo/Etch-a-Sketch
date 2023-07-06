b63a0b206a668d06845caf22dd3384cf21e52330
AriCohelo <ariel.cohelo@gmail.com>
2023-07-06 03:02:46 +0200

    Agregados los divos redondos que van a ser botones y realizado el esquema general del diseño estetiico. Tambien cambie la dispocicion del slider que quedara vertical y he investigado sobre como cambiar los colores y la forma del slider

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
 
-const container = document.querySelector('#container')
+const gridContainer = document.querySelector('#gridContainer')
 function gridCreator(num) {
 
-  const squareSize = 80 / num;
+  const squareSize = 70 / num;
   for (let i = 0; i < num; i++) {
     let rowDiv = document.createElement('div');
     rowDiv.classList.add('rowDiv');
-    container.appendChild(rowDiv);
+    gridContainer.appendChild(rowDiv);
 
     for (let j = 0; j < num; j++) {
       let columnDiv = document.createElement('div');
@@ -36,26 +36,28 @@ function handleMouseUp() {
 function changeColor(e) {
   if (isMouseDown) {
     if (e.target.classList.contains('columnDiv')) {
-      e.target.style.backgroundColor = 'red';
+      e.target.style.backgroundColor = 'black';
     }
   }
 }
 
-
-// let gridButton = document.querySelector('.gridSquares')
-// gridButton.addEventListener('click', function () {
-//   let value = prompt('how many squares');
-//   let squares = document.querySelectorAll('.columnDiv');
-//   squares.forEach(square => square.remove())
-//   gridCreator(value);
-
-// })
-
 let slider = document.querySelector('.slider');
+function sliderStyle() {
+
+  let sliderText = document.querySelector('.sliderText');
+  sliderText.textContent = `${slider.value} X ${slider.value}`;
+  sliderText.style.color = '#8d8d8e';
+  sliderText.style.textAlign = 'center';
+  sliderText.style.marginTop = '85px'
+}
+sliderStyle();
+
 slider.addEventListener('input', function () {
-  // let value = 'input'
   let squares = document.querySelectorAll('.columnDiv');
-  squares.forEach(square => square.remove())
+  squares.forEach(square => {
+    square.remove();
+    sliderStyle();
+  })
   gridCreator(this.value);
 })
 
