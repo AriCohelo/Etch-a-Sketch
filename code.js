ff5b4b2037d0c909002f53895fafdfd8abb0bb2c
AriCohelo <ariel.cohelo@gmail.com>
2023-07-08 16:08:23 +0200

    prueba checkout

--- code.js
+++ code.js
@@ -18,16 +18,41 @@ function gridCreator(num) {
   }
 }
 gridCreator(32)
-// let divs = document.querySelectorAll('div')
-// divs.forEach(div => {
-//   div.addEventListener('mouseover', changeColor);
+
+let isMouseDown = false;
+function handleMouseDown() {
+  isMouseDown = true;
+}
+function handleMouseUp() {
+  isMouseDown = false;
+}
+
+
+// let cells = document.querySelectorAll('.cell')
+// cells.forEach(elm => {
+//   elm.addEventListener('mouseover', gayColor);
 // });
-// const gayColors = ['228, 3, 3', '255, 140, 0', '	255, 237, 0', '0, 128, 38', '36, 64, 142', '115, 41, 130']
 
-// function changeColor(e) {
-//   const random = Math.floor(Math.random() * gayColors.length);
-//   const gayColor = `rgb(${gayColors[random]})`;
 
-//   e.target.style.backgroundColor = gayColor
-//   console.log('mouseover')
-// }
+function hover(e) {
+
+  let squares = document.querySelectorAll('.cell');
+  squares.forEach((square) => {
+    square.addEventListener('mousedown', handleMouseDown);
+    square.addEventListener('mousemove', () => { gayColor(e) });
+    square.addEventListener('mouseup', handleMouseUp);
+  });
+  console.log('click hover')
+}
+
+function gayColor(e) {
+  const gayColors = ['228, 3, 3', '255, 140, 0', '	255, 237, 0', '0, 128, 38', '36, 64, 142', '115, 41, 130']
+  const random = Math.floor(Math.random() * gayColors.length);
+  const gayColor = `rgb(${gayColors[random]})`;
+  e.target.style.backgroundColor = gayColor
+}
+
+let clickCells = document.querySelectorAll('.cell');
+clickCells.forEach((square) => {
+  square.addEventListener('mousedown', hover)
+});
